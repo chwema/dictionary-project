9575d2762305b421c7d895bd66d8ede4c1725b27
chwema <101362399+chwema@users.noreply.github.com>
2022-05-16 14:50:34 -0500

    Added a search engine and a Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="Dictionary logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={logo}
+            className="App-logo img-fluid"
+            alt="Dictionary logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Chido</footer>
+      </div>
     </div>
   );
 }
